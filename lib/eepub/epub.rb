71f27a91266bf542c85bfdd2c4e9a74637a58f8e
Keita Urashima <ursm@ursm.jp>
2017-10-22 02:32:09 +0900

    Refactoring

--- lib/eepub/epub.rb
+++ lib/eepub/epub.rb
@@ -4,6 +4,21 @@ require 'zip'
 
 module Eepub
   class Epub
+    XMLNS = {
+      'container' => 'urn:oasis:names:tc:opendocument:xmlns:container',
+      'dc'        => 'http://purl.org/dc/elements/1.1/',
+    }
+
+    using Module.new {
+      refine Hash do
+        def slice(*keys)
+          keys.each_with_object({}) {|k, h|
+            h[k] = self[k]
+          }
+        end
+      end
+    } unless {}.respond_to?(:slice)
+
     def initialize(path)
       @path = path
     end
@@ -14,7 +29,7 @@ module Eepub
       @title ||= Zip::File.open(@path) {|zip|
         package_xml = REXML::Document.new(package_entry(zip).get_input_stream)
 
-        REXML::XPath.first(package_xml, '//dc:title', 'dc' => 'http://purl.org/dc/elements/1.1/').text
+        REXML::XPath.first(package_xml, '//dc:title', XMLNS.slice('dc')).text
       }
     end
 
@@ -26,7 +41,7 @@ module Eepub
       Zip::File.open to do |zip|
         entry      = package_entry(zip)
         xml        = REXML::Document.new(entry.get_input_stream)
-        title_node = REXML::XPath.first(xml, '//dc:title', 'dc' => 'http://purl.org/dc/elements/1.1/')
+        title_node = REXML::XPath.first(xml, '//dc:title', XMLNS.slice('dc'))
 
         title_node.text = title
 
@@ -43,7 +58,7 @@ module Eepub
     def package_entry(zip)
       container_entry = zip.find_entry('META-INF/container.xml')
       container_xml   = REXML::Document.new(container_entry.get_input_stream)
-      path            = REXML::XPath.first(container_xml, '//container:rootfile/@full-path', 'container' => 'urn:oasis:names:tc:opendocument:xmlns:container').value
+      path            = REXML::XPath.first(container_xml, '//container:rootfile/@full-path', XMLNS.slice('container')).value
 
       zip.find_entry(path)
     end
