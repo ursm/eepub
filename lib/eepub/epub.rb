15fbefc5521969add0b8e1d2cbdb4ce34744d536
Keita Urashima <ursm@ursm.jp>
2017-10-22 17:10:43 +0900

    Remove extra block

--- lib/eepub/epub.rb
+++ lib/eepub/epub.rb
@@ -44,10 +44,7 @@ module Eepub
 
         REXML::XPath.first(rootfile_doc, '//dc:title', XMLNS.slice('dc')).text = title
 
-        zip.get_output_stream rootfile_entry.name do |stream|
-          rootfile_doc.write stream
-        end
-
+        zip.get_output_stream rootfile_entry.name, &rootfile_doc.method(:write)
         zip.commit
       end
     end
